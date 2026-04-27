d6efc4ce4d66dc52d0ddaf16fef565f8fab7557c
Joseph Westwood <joe@indiespring.com>
2026-04-27 10:41:39 +0100

    Moved public files into public/ directory

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 const DATA_FILE = path.join(__dirname, 'data', 'highscores.json');
 
 app.use(express.json());
-app.use(express.static('.'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 function readScores() {
     try {
